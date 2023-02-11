593d3209c30c45d1b53d4cbd26846954542ff8af
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2023-02-11 11:44:25 +0530

    Backend created

---
+++ server.js
@@ -0,0 +1,86 @@
+//importing
+import express from "express";
+import mongoose from 'mongoose';
+import Messages from './dbMessages.js';
+import Pusher from "pusher";
+
+//app config
+const app = express();
+const port = process.env.PORT || 9000;
+const pusher = new Pusher({
+  appId: "1552814",
+  key: "92497045b7862b1ae285",
+  secret: "11e31e2c2df852f4c260",
+  cluster: "ap2",
+  useTLS: true
+});
+
+//middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+})
+
+//db config
+const connection_url = "mongodb+srv://admin:UJkdkCAhi878T6vY@cluster0.ihrxmrf.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//????
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('DB connected');
+
+  const messageCollection = db.collection('messagecontents');
+  const changeStream = messageCollection.watch();
+
+  changeStream.on('change', change => {
+    console.log(change);
+
+    if (change.operationType === 'insert') {
+      const messageDetails = change.fullDocument;
+      pusher.trigger('messages', 'inserted', {
+        name: messageDetails.name,
+        message: messageDetails.message
+      })
+    }
+    else {
+      console.log('error triggering pusher');
+    }
+  })
+});
+
+
+//api routes
+app.get('/', (req, res) => res.status(200).send('Hello World!'));
+
+app.get('/messages/sync', (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(200).send(data);
+    }
+  })
+})
+
+app.post('/messages/new', (req, res) => {
+  const messageData = req.body;
+
+  Messages.create(messageData, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+//listening
+app.listen(port, () => console.log(`listening on localhost:${port}`));
